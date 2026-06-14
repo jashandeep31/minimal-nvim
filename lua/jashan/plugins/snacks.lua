7edd5cde980fe33988ed94f20c49fd81a0f8a6f5
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-14 15:50:52 +0530

    feat: multi line removed

--- lua/jashan/plugins/snacks.lua
+++ lua/jashan/plugins/snacks.lua
@@ -1,130 +1,123 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	---@type snacks.Config
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-		bigfile = { enabled = false },
-		dashboard = { enabled = false },
-		explorer = {
-			enabled = false,
-		},
-		indent = { enabled = false },
-		input = { enabled = true },
-		picker = { enabled = true },
-		notifier = { enabled = false },
-		quickfile = { enabled = false },
-		scope = { enabled = false },
-		scroll = { enabled = false },
-		statuscolumn = { enabled = false },
-		words = { enabled = false },
-		bufdelete = {},
-		lazygit = {},
-		terminal = {},
-		scratch = {},
-	},
-	keys = {
-		{
-			"<leader>gs",
-			function()
-				Snacks.picker.git_status()
-			end,
-			desc = "Git Status (Changed Files)",
-		},
-		{
-			"<leader>gh",
-			function()
-				Snacks.picker.git_diff()
-			end,
-			desc = "Git Diff Hunks",
-		},
-		{
-			"<leader>gb",
-			function()
-				Snacks.picker.git_branches()
-			end,
-			desc = "Git Branches",
-		},
-		{
-			"<leader>gl",
-			function()
-				Snacks.picker.git_log()
-			end,
-			desc = "Git Log",
-		},
-		{
-			"<leader>gL",
-			function()
-				Snacks.picker.git_log_line()
-			end,
-			desc = "Git Log Current Line",
-		},
-		{
-			"<leader>gS",
-			function()
-				Snacks.picker.git_stash()
-			end,
-			desc = "Git Stash",
-		},
-		{
-			"<leader>gf",
-			function()
-				Snacks.picker.git_log_file()
-			end,
-			desc = "Git Log Current File",
-		},
-		{
-			"<leader>gc",
-			function()
-				Snacks.picker.git_log_file({
-					confirm = function(picker, item)
-						picker:close()
-						if not item or not item.commit then
-							return
-						end
-						local file = item.file or vim.api.nvim_buf_get_name(0)
-						if file == "" then
-							return
-						end
-						vim.schedule(function()
-							vim.cmd("DiffviewOpen " .. item.commit .. "^! -- " .. vim.fn.fnameescape(file))
-						end)
-					end,
-				})
-			end,
-			desc = "Pick Commit Diff (File)",
-		},
-		{
-			"<leader>lg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit",
-		},
-		{
-			"<leader>tx",
-			function()
-				Snacks.bufdelete()
-			end,
-			desc = "Close buffer (Snacks)",
-		},
-		{
-			"<leader>u",
-			function()
-				Snacks.picker.undo()
-			end,
-			desc = "Undo History",
-		},
-		{
-			[[<c-\>]],
-			function()
-				Snacks.terminal(nil, { win = { border = "rounded", position = "float" } })
-			end,
-			mode = { "n", "t" },
-			desc = "Toggle Floating Terminal",
-		},
-	},
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = false },
+    dashboard = { enabled = false },
+    explorer = {
+      enabled = false,
+    },
+    indent = { enabled = false },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = false },
+    quickfile = { enabled = false },
+    scope = { enabled = false },
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    words = { enabled = false },
+    bufdelete = {},
+    lazygit = {},
+    terminal = {},
+    scratch = {},
+  },
+  keys = {
+    {
+      "<leader>gs",
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = "Git Status (Changed Files)",
+    },
+    {
+      "<leader>gh",
+      function()
+        Snacks.picker.git_diff()
+      end,
+      desc = "Git Diff Hunks",
+    },
+    {
+      "<leader>gb",
+      function()
+        Snacks.picker.git_branches()
+      end,
+      desc = "Git Branches",
+    },
+    {
+      "<leader>gl",
+      function()
+        Snacks.picker.git_log()
+      end,
+      desc = "Git Log",
+    },
+    {
+      "<leader>gL",
+      function()
+        Snacks.picker.git_log_line()
+      end,
+      desc = "Git Log Current Line",
+    },
+    {
+      "<leader>gS",
+      function()
+        Snacks.picker.git_stash()
+      end,
+      desc = "Git Stash",
+    },
+    {
+      "<leader>gc",
+      function()
+        Snacks.picker.git_log_file({
+          confirm = function(picker, item)
+            picker:close()
+            if not item or not item.commit then
+              return
+            end
+            local file = item.file or vim.api.nvim_buf_get_name(0)
+            if file == "" then
+              return
+            end
+            vim.schedule(function()
+              vim.cmd("DiffviewOpen " .. item.commit .. "^! -- " .. vim.fn.fnameescape(file))
+            end)
+          end,
+        })
+      end,
+      desc = "Pick Commit Diff (File)",
+    },
+    {
+      "<leader>lg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit",
+    },
+    {
+      "<leader>tx",
+      function()
+        Snacks.bufdelete()
+      end,
+      desc = "Close buffer (Snacks)",
+    },
+    {
+      "<leader>u",
+      function()
+        Snacks.picker.undo()
+      end,
+      desc = "Undo History",
+    },
+    {
+      [[<c-\>]],
+      function()
+        Snacks.terminal(nil, { win = { border = "rounded", position = "float" } })
+      end,
+      mode = { "n", "t" },
+      desc = "Toggle Floating Terminal",
+    },
+  },
 }
