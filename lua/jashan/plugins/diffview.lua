9ac8aa5b1434e6f93c5293c9751efab833f89001
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-14 12:11:18 +0530

    feat: git and multi line

--- lua/jashan/plugins/diffview.lua
+++ lua/jashan/plugins/diffview.lua
@@ -3,8 +3,8 @@ return {
 	event = "BufReadPre",
 	cmd = { "DiffviewOpen", "DiffviewFileHistory" },
 	keys = {
-		{ "<leader>gd", "<cmd>DiffviewFileHistory %<CR>", desc = "Diff File History" },
-		{ "<leader>gD", "<cmd>DiffviewOpen<CR>", desc = "Diff View (Global)" },
-		{ "<leader>gx", "<cmd>DiffviewClose<CR>", desc = "Close Diff View" },
+		{ "<leader>ld", "<cmd>DiffviewFileHistory %<CR>", desc = "Diff File History" },
+		{ "<leader>lD", "<cmd>DiffviewOpen<CR>", desc = "Diff View (Global)" },
+		{ "<leader>lx", "<cmd>DiffviewClose<CR>", desc = "Close Diff View" },
 	},
 }
--- lua/jashan/plugins/snacks.lua
+++ lua/jashan/plugins/snacks.lua
@@ -28,6 +28,55 @@ return {
 	},
 	keys = {
 		{
+			"<leader>gs",
+			function()
+				Snacks.picker.git_status()
+			end,
+			desc = "Git Status (Changed Files)",
+		},
+		{
+			"<leader>gh",
+			function()
+				Snacks.picker.git_diff()
+			end,
+			desc = "Git Diff Hunks",
+		},
+		{
+			"<leader>gb",
+			function()
+				Snacks.picker.git_branches()
+			end,
+			desc = "Git Branches",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.picker.git_log()
+			end,
+			desc = "Git Log",
+		},
+		{
+			"<leader>gL",
+			function()
+				Snacks.picker.git_log_line()
+			end,
+			desc = "Git Log Current Line",
+		},
+		{
+			"<leader>gS",
+			function()
+				Snacks.picker.git_stash()
+			end,
+			desc = "Git Stash",
+		},
+		{
+			"<leader>gf",
+			function()
+				Snacks.picker.git_log_file()
+			end,
+			desc = "Git Log Current File",
+		},
+		{
 			"<leader>gc",
 			function()
 				Snacks.picker.git_log_file({
