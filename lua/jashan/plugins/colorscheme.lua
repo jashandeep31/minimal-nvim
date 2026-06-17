6c013f32c213e711a98fe9b77be6dc542d64183a
Jashandeep Singh <jashandeep.dev@gmail.com>
2026-06-17 15:05:29 +0530

    feat: color theme change

--- lua/jashan/plugins/colorscheme.lua
+++ lua/jashan/plugins/colorscheme.lua
@@ -1,36 +1,51 @@
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	name = "catppuccin",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.cmd.colorscheme("catppuccin")
-	-- 	end,
-	-- },
-	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- Optionally configure and load the colorscheme
-			-- directly inside the plugin declaration.
-			vim.g.gruvbox_material_enable_italic = true
-			vim.cmd.colorscheme("gruvbox-material")
-			-- hard
-			vim.g.gruvbox_material_background = "hard"
-		end,
-	},
-	-- {
-	-- 	"Shatur/neovim-ayu",
-	-- 	name = "ayu",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("ayu").setup({
-	-- 			mirage = false, -- false = dark, true = mirage variant
-	-- 			terminal = true,
-	-- 			overrides = {},
-	-- 		})
-	-- 		require("ayu").colorscheme()
-	-- 	end,
-	-- },
+  -- {
+  -- 	"catppuccin/nvim",
+  -- 	name = "catppuccin",
+  -- 	priority = 1000,
+  -- 	config = function()
+  -- 		vim.cmd.colorscheme("catppuccin")
+  -- 	end,
+  -- },
+  -- {
+  -- 	"sainnhe/gruvbox-material",
+  -- 	lazy = false,
+  -- 	priority = 1000,
+  -- 	config = function()
+  -- 		-- Optionally configure and load the colorscheme
+  -- 		-- directly inside the plugin declaration.
+  -- 		vim.g.gruvbox_material_enable_italic = true
+  -- 		vim.cmd.colorscheme("gruvbox-material")
+  -- 		-- hard
+  -- 		vim.g.gruvbox_material_background = "hard"
+  -- 	end,
+  -- }
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "storm",     -- Choices: "storm", "moon", "night", "day"
+      transparent = false, -- Enable transparent background
+      terminal_colors = true,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
+  --
+  -- {
+  -- 	"Shatur/neovim-ayu",
+  -- 	name = "ayu",
+  -- 	priority = 1000,
+  -- 	config = function()
+  -- 		require("ayu").setup({
+  -- 			mirage = false, -- false = dark, true = mirage variant
+  -- 			terminal = true,
+  -- 			overrides = {},
+  -- 		})
+  -- 		require("ayu").colorscheme()
+  -- 	end,
+  -- },
 }
